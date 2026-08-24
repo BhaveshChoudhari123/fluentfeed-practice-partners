ee9e9b272bb57b57942197d8e218439540b8dd1e
BhaveshChoudhari <bhaveshchoudhari547@gmail.com>
2026-08-24 21:54:56 +0530

    Fix production user API

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,15 +1,35 @@
 import { User } from '../models/User.js';
 
+function escapeRegex(value) {
+  return String(value).replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
 export async function listUsers(req, res, next) {
   try {
     const { englishLevel, learningGoal, country, search } = req.query;
-    const query = {};
-    if (englishLevel) query.englishLevel = englishLevel;
-    if (learningGoal) query.learningGoal = learningGoal;
-    if (country) query.country = new RegExp(`^${String(country).replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}$`, 'i');
-    if (search) query.name = new RegExp(String(search).replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'i');
 
-    const users = await User.find(query).sort({ createdAt: -1 }).limit(100);
+    const query = {};
+
+    if (englishLevel) {
+      query.englishLevel = englishLevel;
+    }
+
+    if (learningGoal) {
+      query.learningGoal = learningGoal;
+    }
+
+    if (country) {
+      query.country = new RegExp(`^${escapeRegex(country)}$`, 'i');
+    }
+
+    if (search) {
+      query.name = new RegExp(escapeRegex(search), 'i');
+    }
+
+    const users = await User.find(query)
+      .sort({ createdAt: -1 })
+      .limit(100);
+
     res.json({ users });
   } catch (error) {
     next(error);
